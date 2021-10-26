eddd2398e8f0cab9a5ba574d0a417944e9e0dde9
Ed Gallyot <edgallyot@gmail.com>
2021-10-26 17:06:50 +0100

    Adding in all files

---
+++ cmake-build-debug/CMakeFiles/Physical_Modelling_Percussion_Example_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Physical_Modelling_Percussion_Example_1.dir/main.c.o"
+  "Physical_Modelling_Percussion_Example_1"
+  "Physical_Modelling_Percussion_Example_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Physical_Modelling_Percussion_Example_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+#define SOUND_DECAY 0.95
+#define SYSTEM_DECAY 0.999
+#define SHELL_FREQ 3200.0
+#define SHELL_RESO 0.96
+#define TWO_PI 6.28318530718
+#define SRATE 22050.0
+
+int main (int argc, char* argv[])
+{
+    FILE* file_out;
+
+    double temp = 0;
+    double shakeEnergy = 0.0;
+    double sndLevel = 0.0;
+    double gain;
+    double input = 0.0;
+    double output[2] = {0.0, 0.0}, coeffs[2];
+
+    long i;
+    long num_beans = 64;
+    short data;
+    extern double noise_tick ();
+
+    gain = log (num_beans) / log (4) * 40.0;
+
+    if (file_out = fopen (argv[1], "wb"))
+    {
+        printf ("Stink!\n");
+        coeffs[0] = - SHELL_RESO * 2.0 * cos(SHELL_FREQ * TWO_PI * SRATE);
+
+    }
+
+    return 0;
+
+
+}
